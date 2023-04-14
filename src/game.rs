c16e7d991e385d21a364d3717049f2f1ff2fcd59
Sören Michaels <soeren.michaels@gmail.com>
2023-04-14 02:00:03 +0200

    feat: game stuff

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,45 @@
-use actix::{Actor, Context};
+use std::collections::HashMap;
 
-pub struct Game;
+use actix::{Actor, Context, Handler, Message};
+
+enum GameState {
+    Image,
+    AfterImage,
+    Results,
+}
+struct Image {
+    url: String,
+    result: u16,
+    description: String,
+    // string -> sender
+    guesses: HashMap<String, u16>,
+    scores: HashMap<String, f64>,
+}
+
+pub struct Game {
+    state: GameState,
+    images: Vec<Image>,
+    round_number: u8,
+}
 
 impl Actor for Game {
     type Context = Context<Self>;
+}
+
+struct TwitchMsg {
+    msg: String,
+    author: String,
+    author_id: String,
+}
+
+impl Message for TwitchMsg {
+    type Result = ();
+}
+
+impl Handler<TwitchMsg> for Game {
+    type Result = ();
+
+    fn handle(&mut self, msg: TwitchMsg, ctx: &mut Self::Context) -> Self::Result {
+        todo!()
+    }
 }

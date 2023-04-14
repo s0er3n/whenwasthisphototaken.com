9166a3abed7ccc9594cbc5888ff51f00e975b4cf
Sören Michaels <soeren.michaels@gmail.com>
2023-04-14 11:23:06 +0200

    parsing message

--- src/game.rs
+++ src/game.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use actix::{Actor, Context, Handler, Message};
 
 use twitch_irc::message::PrivmsgMessage;
+use regex::Regex;
 
 enum GameState {
     Image,
@@ -36,6 +37,18 @@ pub struct TwitchMsg {
 
 impl Message for TwitchMsg {
     type Result = ();
+}
+
+impl TwitchMsg {
+    fn find_year(&self) -> Option<u16>{
+    let re = Regex::new(r"/\b(19\d{2}|20[0|1]\d|202[0-2])\b/gm").unwrap();
+    if let Some(cap) = re.captures(&self.msg) {
+        if let Some(year) = cap.get(0) {
+            return year.as_str().parse::<u16>().ok();
+        }
+    }
+    None
+    }
 }
 
 impl From<PrivmsgMessage> for TwitchMsg {
